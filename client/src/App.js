b64c96c50956c73a1949b212c6d5744634098966
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2023-02-22 17:43:59 +0100

    Add Bootstrap. Create app routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
-import styles from './App.module.css';
+import { Route, Routes } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Container from 'react-bootstrap/Container';
 
 const App = () => {
   return (
-    <div className={styles.container}>
-      Witam w Alikacji
-    </div>
+    <Container>
+      {/* Header */}
+      <Routes>
+        {/* <Route path={`/`} element={<Home />} />
+        <Route path={`/ads/:id`} element={<Ad />} />
+        <Route path={`/ads/add`} element={<AdAdd />} />
+        <Route path={`/ads/edit/:id`} element={<AdEdit />} />
+        <Route path={`/ads/remove/:id`} element={<AdRemove />} />
+        <Route path={`/search/:searchPhrase`} element={<Search />} />
+        <Route path={`/login`} element={<Login />} />
+        <Route path={`/register`} element={<Register />} />
+        <Route path={`/logout`} element={<Logout />} />
+        <Route path={`*`} element={<NotFound />} /> */}
+      </Routes>
+    </Container>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-
+import { BrowserRouter } from 'react-router-dom';
 import store from './Redux/store';
 
 import App from './App';
@@ -10,11 +10,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    {/* BrowserRouter */}
-    <Provider store={store}>
-      <App />
-    </Provider>
-    {/*  */}
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
